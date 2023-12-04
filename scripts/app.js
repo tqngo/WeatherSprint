dd69dc183d240918ab1676cde86a2fd862687bce
tqngo <tommyquocngo@gmail.com>
2023-12-04 12:50:52 -0800

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+navigator.geolocation.getCurrentPosition(success, errorFunc);
+
+
+function success(position){
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+}
+function errorFunc(error){
+    console.log(error.message);
+}
+
+
+
+
+
+
+
+
+
+async function getWeather(){
+    const promise = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.961632&lon=-121.275604&appid=6323ec9d5f95a74e8be8d2dcf21d1e02");
+    const data = await promise.json();
+}
+
+getWeather();
