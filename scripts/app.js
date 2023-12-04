6a2f3caa784e78608bfc555bc6826fe2e4e79cdb
Tommy Ngo <tommyquocngo@gmail.com>
2023-12-04 14:59:45 -0800

    fetch

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
+// let locName = document.getElementById("locName");
+// let currentTemp = document.getElementById("currentTemp");
+// let feelsLike = document.getElementById("feelsLike")
+// let maxTemp = document.getElementById("maxTemp");
+// let windSpeed = document.getElementById("windSpeed")
+
 navigator.geolocation.getCurrentPosition(success, errorFunc);
 
 
@@ -12,14 +18,19 @@ function errorFunc(error){
 
 
 
-
-
-
-
-
 async function getWeather(){
-    const promise = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=37.961632&lon=-121.275604&appid=6323ec9d5f95a74e8be8d2dcf21d1e02");
+    const promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.961632&lon=-121.275604&units=imperial&appid=6323ec9d5f95a74e8be8d2dcf21d1e02');
     const data = await promise.json();
+
+    console.log(data)
+    locName.innerText = data.name
+    currentTemp.innerText = Math.round(data.main.temp)
+    weatherCon.innerText = data.weather[0].description
+    feelsLike.innerText = Math.round(data.main.feels_like)
+    maxTemp.innerText = Math.round(data.main.temp_max)
+    minTemp.innerText = Math.round(data.main.temp_min)
+    windSpeed.innerText = Math.round(data.wind.speed)
+
 }
 
 getWeather();
