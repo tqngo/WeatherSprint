31f2bc3d23370e0aa5b8984ef4178839d7e0c25c
Tommy Ngo <tommyquocngo@gmail.com>
2023-12-04 16:10:57 -0800

    day 1

--- scripts/app.js
+++ scripts/app.js
@@ -22,7 +22,7 @@ async function getWeather(){
     const promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=37.961632&lon=-121.275604&units=imperial&appid=6323ec9d5f95a74e8be8d2dcf21d1e02');
     const data = await promise.json();
 
-    locName = data.name
+    locName.innerText = data.name
     currentTemp.innerText = Math.round(data.main.temp)
     weatherCon.innerText = data.weather[0].description
     feelsLike.innerText = Math.round(data.main.feels_like)
